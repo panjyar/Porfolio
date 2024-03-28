e64b6b3b9465f9fd63c3e8a8c2a934fbae72e0ae
panjyar <pranjiyarkehuli@gmail.com>
2024-03-29 01:06:39 +0530

    Island and sky models done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
 
         {/*Navlink react-router-dom*/} 
         <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-            <p className="blue-gradient_text">PKP</p>
+            <p className="blue-gradient_text">PKP </p>
         </NavLink>
 
         <nav className="flex text-lg gap-7 font-medium">
-            <NavLink to = "/" className={({ isActive }) => isActive ?'text-blue-500' : 'text-black'}>
+            <NavLink to = "/" className={({ isActive }) => isActive ?'text-white' : 'text-black'}>
                 About 
             </NavLink>
 
